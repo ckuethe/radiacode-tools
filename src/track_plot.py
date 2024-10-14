1fea3f6093fada441b7dd8f8963e0351f8474df1
Chris Kuethe <chris.kuethe@gmail.com>
2024-10-13 20:28:51 -0700

    make this much prettier

--- src/track_plot.py
+++ src/track_plot.py
@@ -8,7 +8,8 @@
 import os
 from argparse import ArgumentParser, Namespace
 from datetime import datetime, timezone
-from typing import List
+from math import ceil, floor, log10, sqrt
+from typing import Any, Dict, List
 
 import kaleido as kd  # plotly does this automatically, but this is a dependency check
 import pandas as pd  # kaleido does this automatically, but this is a dependency check
@@ -16,7 +17,7 @@ import plotly.express as px
 
 # Load the RadiaCode tools
 from rcfiles import RcTrack
-from rctypes import TrackPoint
+from rctypes import Number, TrackPoint
 
 # Figure out what the local timezone is, once, because I use it so often
 localtz = datetime.now(timezone.utc).astimezone().tzinfo
@@ -37,6 +38,12 @@ def get_args() -> Namespace:
             return rv
         raise ValueError()
 
+    def Geometry(s: str):
+        n = s.strip().split("x")
+        if len(n) != 2:
+            raise ValueError()
+        return PositiveInt(n[0]), PositiveInt(n[1])
+
     ap = ArgumentParser()
     ap.add_argument("-o", "--output-file", type=str, metavar="FILE", help="[<input_file>.png]")
     ap.add_argument(
@@ -49,12 +56,13 @@ def get_args() -> Namespace:
     ap.add_argument("-d", "--downsample", type=PositiveInt, default=16, help="[%(default)s]")
     ap.add_argument("-r", "--min-count-rate", type=PositiveFloat)
     ap.add_argument("-R", "--max-count-rate", type=PositiveFloat)
+    ap.add_argument("-g", "--geometry", type=Geometry, default="1600x1024", help="Image size [%(default)s]")
     ap.add_argument("--interactive", default=False, action="store_true", help="Open an interactive plot in a browser")
     ap.add_argument("--opacity", type=PositiveFloat, default=0.25, help="Marker opacity [%(default)s]")
     ap.add_argument(
         "--palette",
         default="turbo",
-        choices=["magma", "rainbow", "thermal", "turbo", "veridis"],
+        choices=["magma", "rainbow", "thermal", "turbo", "viridis"],
         help="see https://plotly.com/python/builtin-colorscales/ [%(default)s]",
     )
     ap.add_argument("--renderer", choices=["plotly", "hvplot"], default="plotly")
@@ -72,6 +80,10 @@ def rangefinder(l, a=None):
     if a:
         return rangefinder([getattr(x, str(a)) for x in l])
     return (min(l), max(l))
+
+
+def mean(l: List[Number]) -> float:
+    return sum(l) / len(l)
 
 
 def range_probe(l: List[TrackPoint]):
@@ -93,9 +105,6 @@ def downsample_trackpoints(l: List[TrackPoint], factor=4) -> List[TrackPoint]:
     rv = []
     samples: List[TrackPoint] = []
 
-    def _mean(l):
-        return sum(l) / len(l)
-
     def _sl2tp(samples):
         "Sample List to TrackPoint"
         dt = [x.datetime.timestamp() for x in samples]
@@ -105,12 +114,12 @@ def downsample_trackpoints(l: List[TrackPoint], factor=4) -> List[TrackPoint]:
         doserate = [x.doserate for x in samples]
         accuracy = [x.accuracy for x in samples]
 
-        dt = datetime.fromtimestamp(_mean(dt), localtz)
-        lat = _mean(lat)
-        lon = _mean(lon)
-        doserate = _mean(doserate)
-        countrate = _mean(countrate)
-        accuracy = _mean(accuracy)
+        dt = datetime.fromtimestamp(mean(dt), localtz)
+        lat = mean(lat)
+        lon = mean(lon)
+        doserate = mean(doserate)
+        countrate = mean(countrate)
+        accuracy = mean(accuracy)
 
         return TrackPoint(dt, lat, lon, accuracy, doserate, countrate)
 
@@ -123,17 +132,62 @@ def downsample_trackpoints(l: List[TrackPoint], factor=4) -> List[TrackPoint]:
     return rv
 
 
+def map_ctr(bbx: Dict[str, Any]) -> Dict[str, float]:
+    "Normally I'd use a GeoPoint for this, but scatter_mapbox wants a dict"
+    return {
+        "lat": sum(bbx["latitude"]) / 2,
+        "lon": sum(bbx["longitude"]) / 2,
+    }
+
+
+def osm_zoom(bbx: Dict[str, Any]) -> int:
+    dx = bbx["longitude"][1] - bbx["longitude"][0]
+    dy = bbx["latitude"][1] - bbx["latitude"][0]
+    ld = log10(sqrt(dx**2 + dy**2))
+    zl = ceil(-3.27 * ld + 9.32)
+    return zl
+
+
+def render_plotly(tk: RcTrack, args: Namespace, bbx: Dict[str, Any]):
+    zoom = osm_zoom(bbx)
+    fig = px.scatter_mapbox(
+        tk.points,
+        lat="latitude",
+        lon="longitude",
+        hover_name="datetime",
+        zoom=zoom,
+        center=map_ctr(bbx),
+        color="doserate",
+        color_continuous_scale=args.palette,
+        # opacity=0.25,
+        # size="countrate",
+        width=args.geometry[0],
+        height=args.geometry[1],
+    )
+    fig.update_layout(mapbox_style="open-street-map")
+    fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
+    fig.write_image(args.output_file)
+    if args.interactive:
+        fig.show()
+
+
+def render_hvplot(tk: RcTrack, args: Namespace, bbx: Dict[str, Any]):
+    # https://hvplot.holoviz.org/user_guide/Geographic_Data.html
+    # https://hvplot.holoviz.org/getting_started/installation.html
+    # https://examples.holoviz.org/gallery/opensky/opensky.html
+    # https://examples.holoviz.org/gallery/osm/index.html
+    #
+    raise NotImplementedError
+
+
 def main() -> None:
     args = get_args()
     tk = RcTrack(args.input_file)
 
-    bounding_box = range_probe(tk.points)
-    print(bounding_box)
-
     if args.downsample >= 2:
         tk.points = downsample_trackpoints(tk.points, args.downsample)
 
-    # FIXME would a pandas dataframe or parquet be better? can i just
+    # FIXME would a pandas dataframe or parquet be better? would their filters be faster?
     for i in range(len(tk.points)):
         x = list(tk.points[i])
         x[0] = x[0].astimezone(localtz)
@@ -150,37 +204,12 @@ def main() -> None:
         if tk.points[i].accuracy > 15:
             tk.points.pop(i)
 
+    bounding_box = range_probe(tk.points)
+
     if "plotly" == args.renderer:
-        render_plotly(tk, args)
+        render_plotly(tk, args, bounding_box)
     if "hvplot" == args.renderer:
-        render_hvplot(tk, args)
-
-
-def render_plotly(tk: RcTrack, args: Namespace):
-    fig = px.scatter_mapbox(
-        tk.points,
-        lat="latitude",
-        lon="longitude",
-        hover_name="datetime",
-        zoom=14,  # FIXME compute this from bounding box
-        color="doserate",
-        color_continuous_scale=args.palette,
-        # opacity=0.25,
-        # size="countrate",
-        height=900,
-        width=1440,
-    )
-    fig.update_layout(mapbox_style="open-street-map")
-    fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
-    fig.write_image(args.output_file)
-    if args.interactive:
-        fig.show()
-
-    # FIXME also try holoviz? this looks https://examples.holoviz.org/gallery/opensky/opensky.html
-
-
-def render_hvplot(tk: RcTrack, args: Namespace):
-    raise NotImplementedError
+        render_hvplot(tk, args, bounding_box)
 
 
 if __name__ == "__main__":
